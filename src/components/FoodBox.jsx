3f16bfaf33e6bb3de97563b945d273571c6f9aa1
Pau Mesonero <paumesonero@Caitlins-MacBook-Air.local>
2022-08-02 17:27:28 +0200

    little mistake fixed

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function foodBox(props) {
     const { food, onDelete } = props
+
+    let totalCaloriesMath = food.calories * food.servings
     return (
         <div className='each-food-box'>
             <div className='food-name-box'>
@@ -10,7 +12,7 @@ export default function foodBox(props) {
             <img src={food.image} alt={food.name} width='150px' />
             <p>Calories: {food.calories}</p>
             <p>Servings: {food.servings}</p>
-            <p> <strong>Total Calories: {food.calories} kcal</strong></p>
+            <p> <strong>Total Calories: {totalCaloriesMath} kcal</strong></p>
             <button onClick={() => onDelete(food.name)}>Delete</button>
         </div >
     )
