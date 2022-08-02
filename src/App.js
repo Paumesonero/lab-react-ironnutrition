1ebbc59f8e6aad63c6b8375d4dc6b135b34165ea
Pau Mesonero <paumesonero@Caitlins-MacBook-Air.local>
2022-08-02 15:46:09 +0200

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import './App.css';
 import foodData from './foods.json';
 import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
   const [foods, setFoods] = useState(foodData)
+
+  const handleNewFood = (food) => {
+    const foodsCopy = [...foods]
+    foodsCopy.push(food)
+    setFoods(foodsCopy)
+  }
   return (
     <div className="App">
       <h1>Food List</h1>
+      <div>
+        <AddFoodForm newFood={handleNewFood} />
+      </div>
       <div className='main-content-box'>
         {foods.map(el => {
           return <FoodBox food={{ name: el.name, calories: el.calories, image: el.image, servings: el.servings }} />
---
+++ src/components/AddFoodForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+export default function (props) {
+    const { newFood } = props
+    const [name, setName] = useState('')
+    const [image, setImage] = useState('')
+    const [calories, setCalories] = useState('')
+    const [servings, setServings] = useState('')
+
+
+    const handleForm = (e) => {
+        e.preventDefault();
+        const food = {
+            name: name,
+            image: image,
+            calories: parseInt(calories),
+            servings: parseInt(servings)
+        }
+        newFood(food)
+        setName('')
+        setImage('')
+        setCalories('')
+        setServings('')
+    }
+    return (
+        <form action="" onSubmit={handleForm}>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input type="text" id='name' value={name} onChange={(e) => { setName(e.target.value) }} />
+            </div>
+            <div>
+                <label htmlFor="image">Image</label>
+                <input type="text" id='image' value={image} onChange={(e) => { setImage(e.target.value) }} />
+            </div>
+            <div>
+                <label htmlFor="calories">Calories</label>
+                <input type="number" id='calories' value={calories} onChange={(e) => { setCalories(e.target.value) }} />
+            </div>
+            <div>
+                <label htmlFor="servings">Servings</label>
+                <input type="number" id='servings' value={servings} onChange={(e) => { setServings(e.target.value) }} />
+            </div>
+            <button>Create</button>
+        </form>
+
+    )
+}
